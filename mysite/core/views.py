3ee424c4c8b1fbd92b584ad2f3dc56a08aa59b7e
lamaalrajih <alrajihlama@gmail.com>
2019-10-24 08:18:59 -0700

    Initial commit

---
+++ mysite/core/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.views.generic import TemplateView
+
+
+def home(request):
+    count = User.objects.count()
+    return render(request, 'home.html', {
+        'count': count,
+    })
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {
+        'form': form,
+    })
+
+
+@login_required
+def secret_page(request):
+    return render(request, 'secret_page.html')
+
+
+class SecretPage(LoginRequiredMixin, TemplateView):
+    template_name = 'secret_page.html'
